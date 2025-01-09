fe6dffc04978c1b84be0756237940bfcf263ffa2
Borghild <borghild.vikoyr@miles.no>
2025-01-09 13:23:00 +0100

    :art: started new

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,107 +1,23 @@
-import { createAuthStore, defineConfig, PluginOptions } from 'sanity'
-import { media } from 'sanity-plugin-media'
-import { structureTool } from 'sanity/structure'
-import { DatabaseIcon } from '@sanity/icons'
-import { visionTool } from '@sanity/vision'
-import { schemaTypes } from './schemaTypes'
-import { structure } from './deskstructure'
-import { defineDocuments, defineLocations, DocumentLocation, presentationTool } from 'sanity/presentation'
-import { dataset, projectId } from './sanity.cli'
+import {defineConfig, createAuthStore} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
 
-// URL for preview functionality, defaults to localhost:3000 if not set
-const SANITY_STUDIO_PREVIEW_URL = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
+export default defineConfig({
+  name: 'default',
+  title: 'Brand wall',
 
-// Define the home location for the presentation tool
-const homeLocation = {
-  title: 'Home',
-  href: '/',
-} satisfies DocumentLocation
+  projectId: 'l3891ift',
+  dataset: 'development',
 
-// resolveHref() is a convenience function that resolves the URL
-// path for different document types and used in the presentation tool.
-function resolveHref(documentType?: string, slug?: string): string | undefined {
-  switch (documentType) {
-    case 'page':
-      return slug ? `/${slug}` : undefined
-    default:
-      console.warn('Invalid document type:', documentType)
-      return undefined
-  }
-}
+  plugins: [structureTool(), visionTool()],
 
-const getStudioTitle = (dataset: string) => {
-  switch (dataset) {
-    case 'development':
-      return 'Development'
-    default:
-      return 'Production'
-  }
-}
-
-const getConfig = (datasetParam: string, projectIdParam: string, isSecret = false) => ({
-  name: 'brandwall-' + datasetParam,
-  title: 'Studio [' + getStudioTitle(datasetParam) + ']',
-  icon: DatabaseIcon,
-  basePath: '/' + datasetParam,
-  projectId: projectIdParam,
-  dataset: datasetParam,
-  //theme: customTheme,
-  plugins: [
-    // Presentation tool configuration for Visual Editing
-    presentationTool({
-      previewUrl: {
-        origin: SANITY_STUDIO_PREVIEW_URL,
-        previewMode: {
-          enable: '/api/draft-mode/enable',
-        },
-      },
-      resolve: {
-        // The Main Document Resolver API provides a method of resolving a main document from a given route or route pattern. https://www.sanity.io/docs/presentation-resolver-api#57720a5678d9
-        mainDocuments: defineDocuments([
-          {
-            route: '/:slug',
-            filter: `_type == "page" && slug.current == $slug || _id == $slug`,
-          },
-        ]),
-        // Locations Resolver API allows you to define where data is being used in your application. https://www.sanity.io/docs/presentation-resolver-api#8d8bca7bfcd7
-        locations: {
-          settings: defineLocations({
-            locations: [homeLocation],
-            message: 'This document is used on all pages',
-            tone: 'positive',
-          }),
-          page: defineLocations({
-            select: {
-              name: 'name',
-              slug: 'slug.current',
-            },
-            resolve: (doc) => ({
-              locations: [
-                {
-                  title: doc?.name || 'Untitled',
-                  href: resolveHref('page', doc?.slug)!,
-                },
-              ],
-            }),
-          }),
-        },
-      },
-    }),
-    structureTool({
-      structure,
-    }),
-    media(),
-    datasetParam === 'development' && visionTool(),
-    //FotowareAssetSource(),
-    //BrandmasterAssetSource(),
-  ].filter((e) => e) as PluginOptions[],
   schema: {
     types: schemaTypes,
   },
-  document: {},
   auth: createAuthStore({
-    projectId: projectIdParam,
-    dataset: datasetParam,
+    projectId: 'l3891ift',
+    dataset: 'development',
     mode: 'replace',
     redirectOnSingle: true,
     providers: [
@@ -114,17 +30,3 @@ const getConfig = (datasetParam: string, projectIdParam: string, isSecret = fals
     ],
   }),
 })
-
-export default getConfig(dataset, projectId)
-
-/* export default defineConfig({
-  name: 'default',
-  title: 'Brand wall',
-  projectId: 'l3891ift',
-  dataset: 'development',
-  plugins: [structureTool(), visionTool()],
-  schema: {
-    types: schemaTypes,
-  },
-
-}) */
---
+++ studio_old_one/sanity.config.ts
@@ -0,0 +1,88 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+import { structure } from './deskstructure'
+import { presentationTool, defineDocuments, defineLocations, type DocumentLocation } from 'sanity/presentation'
+import { dataset, projectId } from './sanity.cli'
+
+// URL for preview functionality, defaults to localhost:3000 if not set
+const SANITY_STUDIO_PREVIEW_URL = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
+
+// Define the home location for the presentation tool
+const homeLocation = {
+  title: 'Home',
+  href: '/',
+} satisfies DocumentLocation
+
+// resolveHref() is a convenience function that resolves the URL
+// path for different document types and used in the presentation tool.
+function resolveHref(documentType?: string, slug?: string): string | undefined {
+  switch (documentType) {
+    case 'page':
+      return slug ? `/${slug}` : undefined
+    default:
+      console.warn('Invalid document type:', documentType)
+      return undefined
+  }
+}
+
+// Main Sanity configuration
+export default defineConfig({
+  name: 'default',
+  title: 'Brandwall',
+  projectId: projectId,
+  dataset: dataset,
+
+  plugins: [
+    // Presentation tool configuration for Visual Editing
+    /*     presentationTool({
+      previewUrl: {
+        origin: SANITY_STUDIO_PREVIEW_URL,
+        previewMode: {
+          enable: '/api/draft-mode/enable',
+        },
+      },
+      resolve: {
+        // The Main Document Resolver API provides a method of resolving a main document from a given route or route pattern. https://www.sanity.io/docs/presentation-resolver-api#57720a5678d9
+        mainDocuments: defineDocuments([
+          {
+            route: '/:slug',
+            filter: `_type == "page" && slug.current == $slug || _id == $slug`,
+          },
+        ]),
+        // Locations Resolver API allows you to define where data is being used in your application. https://www.sanity.io/docs/presentation-resolver-api#8d8bca7bfcd7
+        locations: {
+          settings: defineLocations({
+            locations: [homeLocation],
+            message: 'This document is used on all pages',
+            tone: 'positive',
+          }),
+          page: defineLocations({
+            select: {
+              name: 'name',
+              slug: 'slug.current',
+            },
+            resolve: (doc) => ({
+              locations: [
+                {
+                  title: doc?.name || 'Untitled',
+                  href: resolveHref('page', doc?.slug)!,
+                },
+              ],
+            }),
+          }),
+        },
+      },
+    }), */
+    structureTool({
+      structure, // Custom studio structure configuration, imported from ./src/structure.ts
+    }),
+    /*     visionTool(), */
+  ],
+
+  // Schema configuration, imported from ./src/schemaTypes/index.ts
+  schema: {
+    types: schemaTypes,
+  },
+})
