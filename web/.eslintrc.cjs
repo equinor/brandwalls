57ced9233abd115ec4f99ac2bc19806507c578c5
Borghild <borghild.vikoyr@miles.no>
2025-01-13 10:30:41 +0100

    :art: eslint workaround

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: ['../.eslint.config.mjs'],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  plugins: [],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        varsIgnorePattern: '^React$',
+      },
+    ],
+    // This rule was so noicy so let's turn it off and see how that works
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+  },
 }
