a236d0fad8b969e6aadda8db6737db129d20cd55
Borghild <borghild.vikoyr@miles.no>
2025-03-05 09:44:36 +0100

    :art: update

--- web/components/core/KeyNumber/KeyNumber.tsx
+++ web/components/core/KeyNumber/KeyNumber.tsx
@@ -12,8 +12,8 @@ export const KeyNumber = forwardRef<HTMLDivElement, KeyNumberProps>(function Key
 ) {
   return (
     <div ref={ref} className="flex h-full w-full flex-col justify-start p-4">
-      <div className="text-6xl font-normal leading-none text-norwegian-woods-100">{keyNumber}</div>
-      <div className="mt-4 max-w-[80%] text-2xl leading-planetary">{label}</div>
+      <div className="text-5xl font-normal leading-none text-norwegian-woods-100">{keyNumber}</div>
+      <div className="mt-4 max-w-[80%] text-xl leading-planetary">{label}</div>
     </div>
   )
 })
